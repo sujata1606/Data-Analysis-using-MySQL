c3c06a022981f81e1af40f29977c8ec25d242f04
Sujata Panchal <80943993+sujata1606@users.noreply.github.com>
2021-07-05 17:43:05 +0530

    Importing data in MySQL

--- Import.sql
+++ Import.sql
@@ -1,5 +1,14 @@
  1. Importing data from CSV to MySQL
- 
     load data infile "C:\Users\Lenovo\OneDrive\Desktop\MySql\data.csv" into table UserDetails columns terminated by "," enclosed by "'" ignore 1 lines;
     
-2. Backing up databases in MySQL.
+ 2. Backing up databases in MySQL.
+   show databases;
+   use demo;
+   exit
+   Bye
+   
+   C:\Program Files\MySQL\MySQL Server 8.0\bin>mysqldump -uroot -p demo > demo_1.sql
+   
+ 
+     
+ 
