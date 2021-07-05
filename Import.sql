aa3694900c454cac3b8a7406701dfc46bda5db90
Sujata Panchal <80943993+sujata1606@users.noreply.github.com>
2021-07-05 17:37:03 +0530

    Create Import.sql

---
+++ Import.sql
@@ -0,0 +1,5 @@
+ 1. Importing data from CSV to MySQL
+ 
+    load data infile "C:\Users\Lenovo\OneDrive\Desktop\MySql\data.csv" into table UserDetails columns terminated by "," enclosed by "'" ignore 1 lines;
+    
+2. Backing up databases in MySQL.
