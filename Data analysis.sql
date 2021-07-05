6a68bc3c238a6886f1f7ecfd59282d57d9e37927
Sujata Panchal <80943993+sujata1606@users.noreply.github.com>
2021-07-05 18:01:27 +0530

    Data Analysis using MySQL

---
+++ Data analysis.sql
@@ -0,0 +1,10 @@
+List of modules covered under data analysis module
+
+1. Counting Rows & Items.
+  select count(*) from UserDetails;
+  select distinct Salary from UserDetails;
+  select count(distinct Salary) from UserDetails;
+
+2. Aggregate Functions (SUM, AVG, STDDEV, MIN, MAX)
+   
+  
