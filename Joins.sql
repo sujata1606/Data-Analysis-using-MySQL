fb95e114f61b8d76a252b2004d0d08141299fd90
Sujata Panchal <80943993+sujata1606@users.noreply.github.com>
2021-07-14 18:18:42 +0530

    Joins

---
+++ Joins.sql
@@ -0,0 +1,75 @@
+JOIN : A JOIN concept which is basically a clause used to combine rows from different tables 
+based on a condition
+
+
+1. INNER JOIN : The INNER JOIN helps to select the matching records from the both tables
+
+
+create database mydemo2;
+
+use mydemo2;
+
+
+CREATE TABLE customer
+   (
+       custid VARCHAR(6),
+       fname VARCHAR(30),
+       mname VARCHAR(30),
+       ltname VARCHAR(30),
+       city VARCHAR(15),
+       mobileno VARCHAR(10),
+       occupation VARCHAR(10),
+       dob DATE,
+	   CONSTRAINT customer_custid_pk PRIMARY KEY(custid)
+         
+   )
+   
+   CREATE TABLE account
+   (
+      acnumber VARCHAR(6),
+      custid  VARCHAR(6),
+      bid VARCHAR(6),
+      opening_balance INT,
+      aod DATE,
+      atype VARCHAR(10),
+      astatus VARCHAR(10),
+      CONSTRAINT account_acnumber_pk PRIMARY KEY(acnumber),
+      CONSTRAINT account_custid_fk FOREIGN KEY(custid) REFERENCES customer(custid)
+      
+      
+    );
+
+
+INSERT INTO customer VALUES('C00001','Ramesh','Chandra','Sharma','Delhi','9543198345','Service','1976-12-06');
+INSERT INTO customer VALUES('C00002','Avinash','Sunder','Minha','Delhi','9876532109','Service','1974-10-16');
+INSERT INTO customer VALUES('C00003','Rahul',null,'Rastogi','Delhi','9765178901','Student','1981-09-26');
+INSERT INTO customer VALUES('C00004','Parul',null,'Gandhi','Delhi','9876532109','Housewife','1976-11-03');
+INSERT INTO customer VALUES('C00005','Naveen','Chandra','Aedekar','Mumbai','8976523190','Service','1976-09-19');
+INSERT INTO customer VALUES('C00006','Chitresh',null,'Barwe','Mumbai','7651298321','Student','1992-11-06');
+INSERT INTO customer VALUES('C00007','Amit','Kumar','Borkar','Mumbai','9875189761','Student','1981-09-06');
+INSERT INTO customer VALUES('C00008','Nisha',null,'Damle','Mumbai','7954198761','Service','1975-12-03');
+INSERT INTO customer VALUES('C00009','Abhishek',null,'Dutta','Kolkata','9856198761','Service','1973-05-22');
+INSERT INTO customer  VALUES('C00010','Shankar',null,'Nair','Chennai','8765489076','Service','1976-07-12');
+
+
+INSERT INTO account VALUES('A00001','C00001','B00001',1000,'2012-12-15','Saving','Active');
+INSERT INTO account VALUES('A00002','C00002','B00001',1000,'2012-06-12','Saving','Active');
+INSERT INTO account VALUES('A00003','C00003','B00002',1000,'2012-05-17','Saving','Active');
+INSERT INTO account VALUES('A00004','C00002','B00005',1000,'2013-01-27','Saving','Active');
+INSERT INTO account VALUES('A00005','C00006','B00006',1000,'2012-12-17','Saving','Active');
+INSERT INTO account VALUES('A00006','C00007','B00007',1000,'2010-08-12','Saving','Suspended');
+INSERT INTO account VALUES('A00007','C00007','B00001',1000,'2012-10-02','Saving','Active');
+INSERT INTO account VALUES('A00008','C00001','B00003',1000,'2009-11-09','Saving','Terminated');
+INSERT INTO account VALUES('A00009','C00003','B00007',1000,'2008-11-30','Saving','Terminated');
+INSERT INTO account VALUES('A00010','C00004','B00002',1000,'2013-03-01','Saving','Active');
+
+select * from customer;
+select * from account;
+
+select count(*) from customer;
+select count(*) from account;
+
+select fname,acnumber from customer inner join account on customer.custid = account.custid;
+select fname from customer inner join account on customer.custid = account.custid; 
+
+
