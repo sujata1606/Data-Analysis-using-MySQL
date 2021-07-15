e5ab96bc178f4982de076036b38193dca65d8a3f
Sujata Panchal <80943993+sujata1606@users.noreply.github.com>
2021-07-15 17:52:37 +0530

    Update Joins.sql

--- Joins.sql
+++ Joins.sql
@@ -78,4 +78,91 @@ select fname from customer inner join account on customer.custid = account.custi
 2. LEFT JOIN : The LEFT JOIN helps to fetch all records from the left table and the matching records 
 from the right table
 
+use mydemo3;
+
+select * from branch;
+select * from customer;
+
+CREATE TABLE account
+   (
+      acnumber VARCHAR(6),
+      custid  VARCHAR(6),
+      bid VARCHAR(6),
+      opening_balance INT,
+      aod DATE,
+      atype VARCHAR(10),
+      astatus VARCHAR(10),
+      CONSTRAINT account_acnumber_pk PRIMARY KEY(acnumber),
+      CONSTRAINT account_custid_fk FOREIGN KEY(custid) REFERENCES customer(custid),
+      CONSTRAINT account_bid_fk FOREIGN KEY(bid) REFERENCES branch(bid) 
+    );
+
+
+select * from customer;
+
+INSERT INTO customer VALUES('C00001','Ramesh','Chandra','Sharma','Delhi','9543198345','Service','1976-12-06');
+INSERT INTO customer VALUES('C00002','Avinash','Sunder','Minha','Delhi','9876532109','Service','1974-10-16');
+INSERT INTO customer VALUES('C00003','Rahul',null,'Rastogi','Delhi','9765178901','Student','1981-09-26');
+INSERT INTO customer VALUES('C00004','Parul',null,'Gandhi','Delhi','9876532109','Housewife','1976-11-03');
+INSERT INTO customer VALUES('C00005','Naveen','Chandra','Aedekar','Mumbai','8976523190','Service','1976-09-19');
+INSERT INTO customer VALUES('C00006','Chitresh',null,'Barwe','Mumbai','7651298321','Student','1992-11-06');
+INSERT INTO customer VALUES('C00007','Amit','Kumar','Borkar','Mumbai','9875189761','Student','1981-09-06');
+INSERT INTO customer VALUES('C00008','Nisha',null,'Damle','Mumbai','7954198761','Service','1975-12-03');
+INSERT INTO customer VALUES('C00009','Abhishek',null,'Dutta','Kolkata','9856198761','Service','1973-05-22');
+INSERT INTO customer  VALUES('C00010','Shankar',null,'Nair','Chennai','8765489076','Service','1976-07-12');
+
+select * from customer;
+
+select * from branch;
+
+INSERT INTO branch VALUES('B00001','Asaf ali road','Delhi');
+INSERT INTO branch VALUES('B00002','New delhi main branch','Delhi');
+INSERT INTO branch VALUES('B00003','Delhi cantt','Delhi');
+INSERT INTO branch VALUES('B00004','Jasola','Delhi');
+INSERT INTO branch VALUES('B00005','Mahim','Mumbai');
+INSERT INTO branch VALUES('B00006','Vile parle','Mumbai');
+INSERT INTO branch VALUES('B00007','Mandvi','Mumbai');
+INSERT INTO branch VALUES('B00008','Jadavpur','Kolkata');
+INSERT INTO branch VALUES('B00009','Kodambakkam','Chennai');
+select * from branch;
+
+INSERT INTO account VALUES('A00001','C00001','B00001',1000,'2012-12-15','Saving','Active');
+INSERT INTO account VALUES('A00002','C00002','B00001',1000,'2012-06-12','Saving','Active');
+INSERT INTO account VALUES('A00003','C00003','B00002',1000,'2012-05-17','Saving','Active');
+INSERT INTO account VALUES('A00004','C00002','B00005',1000,'2013-01-27','Saving','Active');
+INSERT INTO account VALUES('A00005','C00006','B00006',1000,'2012-12-17','Saving','Active');
+INSERT INTO account VALUES('A00006','C00007','B00007',1000,'2010-08-12','Saving','Suspended');
+INSERT INTO account VALUES('A00007','C00007','B00001',1000,'2012-10-02','Saving','Active');
+INSERT INTO account VALUES('A00008','C00001','B00003',1000,'2009-11-09','Saving','Terminated');
+INSERT INTO account VALUES('A00009','C00003','B00007',1000,'2008-11-30','Saving','Terminated');
+INSERT INTO account VALUES('A00010','C00004','B00002',1000,'2013-03-01','Saving','Active');
+
+select * from account;
+
+select count(*) from customer;
+select count(custid) from customer;
+select count(acnumber) from account;
+
+select * from customer
+LEFT JOIN account
+ON account.custid = customer.custid;
+
+select * from account
+LEFT JOIN customer
+ON customer.custid = account.custid;
+
+2. RIGHT JOIN : The RIGHT JOIN helps to fetch all records from the right table and the matching records 
+from the left table.
+
+SELECT * FROM account
+right JOIN customer
+ON  customer.custid = account.custid;
+
+SELECT * FROM customer
+right JOIN account
+ON  account.custid = customer.custid;
+
+3. FULL JOIN : The FULL OUTER JOIN helps to fetch all the records from both the table whether there is 
+a match or not.
+
 
